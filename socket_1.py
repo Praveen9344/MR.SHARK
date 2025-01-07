537e28ae3a14b45b5a20837c5c0b9a6ff63be0af
Praveen9344 <157734087+Praveen9344@users.noreply.github.com>
2025-01-07 22:43:12 +0530

    Update socket_1.py

--- socket_1.py
+++ socket_1.py
@@ -5,7 +5,7 @@ def start_server():
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    host = '157.46.85.248'
+    host = '0.0.0.0'
     port = 5000
 
     server_socket.bind((host, port))
